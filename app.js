c261e5a42dec158acbbb854dd3b134de806e1e29
MrFix1989 <madoc1989@gmail.com>
2019-12-16 20:55:43 +0200

    3.3 функция Number

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ value = String(10);   //String фенкция которая принимает 
 value = String(10+34);
 
 value = (40).toString();  // а это уже метод которые преобразовыввает в стринг
+value = String(true);
+value = String([1,3,5,2,3]);
+value = String({name:'vladimir'});
+value = 345 + '' + 30;
+
+// String to number
+
+value = Number('5745');
+value = Number(true);
+value = Number(null);
+value = Number('true');
+value = Number([2,3,5,6,4]);
+
 
 console.log(value);
 console.log(typeof value);   //определение типа данных которые мы выводим
