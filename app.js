3d0074f918dbe6f72f3c6721b3c6909c3fc2fa10
MrFix1989 <madoc1989@gmail.com>
2019-12-16 20:27:31 +0200

    3.1 Функция String

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
-// однострочные комментарии
-
-/*
-    Многострочные комментари
-*/
-
-/**
- * JSDoc комментарии
- */
-
-/*
-console.table({name: 'Paraska', age:43})
-console.clear()
-console.log({name1: 'Paraska2', age1:41})
-*/
 
 let value;
-value = 10;
+value = String(10);   //String фенкция которая принимает в себя значение любое и оно будет преобразованно непосредственно к строке
 console.log(value);
 console.log(typeof value);   //определение типа данных которые мы выводим
 
