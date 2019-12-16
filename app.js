35735ef4f901bd8e292eab16ea3f3a35e2c9e8c6
MrFix1989 <madoc1989@gmail.com>
2019-12-16 20:07:51 +0200

    3. Способ определить тип данных выводимых в консоль

--- app.js
+++ app.js
@@ -8,7 +8,14 @@
  * JSDoc комментарии
  */
 
+/*
 console.table({name: 'Paraska', age:43})
 console.clear()
 console.log({name1: 'Paraska2', age1:41})
+*/
+
+let value;
+value = 10;
+console.log(value);
+console.log(typeof value);   //определение типа данных которые мы выводим
 
