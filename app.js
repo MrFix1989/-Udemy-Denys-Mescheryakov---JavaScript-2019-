9b700b1fcb1e51ccfe4902e3b74fb200cf404dfd
MrFix1989 <madoc1989@gmail.com>
2019-12-17 02:13:56 +0200

    4.1 операторы вычисления

--- app.js
+++ app.js
@@ -2,3 +2,28 @@
 const num1 = 10;
 const num2 = 78;
 let value;
+
+value = num1 + num2;
+console.log(value);
+
+value = value + 100; //операция суммы и после присвоения
+console.log(value);
+
+value += 100; //коротки вариант предыдушей записи
+console.log(value);
+
+value = 4 % 2; //оператор % показывает остаток от деления
+console.log(value);
+
+value++;     // +1 изменения произойдут на следующей строке
+console.log(value);
+
+value--;     //-1
+console.log(value);
+
+--value;    //-1
+console.log(value);
+
+++value;    // +1 изменения произойдут на этой же строке
+console.log(value);
+
