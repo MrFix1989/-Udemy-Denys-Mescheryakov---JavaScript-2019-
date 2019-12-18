fbc63cece3334f20d8323da6a75b898cb4757053
MrFix1989 <madoc1989@gmail.com>
2019-12-19 00:59:32 +0200

    5. конкатенация

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const str = 'Hello my name is Avganistan';
 
 let value;
 
+value = firstName + lastName;
 
+value = firstName + " " + lastName;
 
+//value = value + 'I am' + age;
+
+value += ' I am ' + age; //сокращенный вариантпрошлой записи
+
+console.log(value);
 
 
