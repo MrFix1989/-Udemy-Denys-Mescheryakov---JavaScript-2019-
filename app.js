3909bd18c566a36dbdc4c865630f08cbfe2147a7
MrFix1989 <madoc1989@gmail.com>
2019-12-20 21:21:45 +0200

    6.1 Старый вариант записи

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const age = 30;
 
 let str;
 
+str = '<ul>' +
+        '<li>' + firstName + '</li>'+
+        '<li>' + lastName + '</li>'+
+        '<li>' + age + '</li>'+
+    '</ul>';
+
 
 console.log(str);
+//document.writeln(str);
 
