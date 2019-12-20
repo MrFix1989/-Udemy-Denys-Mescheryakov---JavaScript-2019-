e76867553bd38e765cfc97529e484777770f6dcd
MrFix1989 <madoc1989@gmail.com>
2019-12-20 21:05:07 +0200

    6 шаблонные строки

--- app.js
+++ app.js
@@ -1,67 +1,9 @@
 const firstName = 'Avganistan';
 const lastName = 'Borisov';
-const age = 34;
-const str = 'Hello my name is Avganistan';
+const age = 30;
 
-let value;
-
-value = firstName + lastName;
-
-value = firstName + " " + lastName;
-
-//value = value + 'I am' + age;
-
-value += ' I am ' + age; //сокращенный вариантпрошлой записи
-
-value = firstName.length; //определяем длинну строки
-value = firstName[2]; // получаем третий символ в строке
-value = firstName[firstName.length - 1]; // способ получить последний символ в строке
-
-value = firstName.toUpperCase();
-
-console.log(value, firstName);
-
-value = firstName.toLowerCase();
-console.log(value, firstName);
-
-value = firstName.concat(' ', lastName);  // метод concat производит конкотенацию
-
-console.log(value);
-
-value = firstName.indexOf('n'); //мтод производит поиск симфола или слова в строке, при этом указывает только первое встречабшееся совпадение
-console.log(value);
-
-value = str.indexOf('n', 10); // таким образом мы указываем сетоду поиск начиная с 10 символа в строке пинуя первое совпадение
-console.log(value);
+let str;
 
 
-value = str.indexOf('!'); // если совпадений нет то метод выводит -1
-console.log(value);
-
-
-value = str.lastIndexOf('n'); // выполняет поиск с самого конца строки
-console.log(value);
-
-
-
-value = str.includes('my'); //метод возвращает нам будевое значение о том есть ли данная строка или нет
-console.log(value);
-
-value = str.includes('MY'); // если значение меет к примеру не тот регистр в котором оно записано то метод вернет false
-console.log(value);
-
-value = str.slice(0,5); // оставляет содержимое между указзаных индексов строки
-console.log(value);
-
-value = str.slice(0,-4);  // минусовое значение удаляет симфолы с конца строки
-console.log(value);
-
-
-value = str.replace('name', 'favorite'); // метод заменяет одно значение строки на другое
-console.log(value);
-
-
-
-
-
+console.log(str);
 
