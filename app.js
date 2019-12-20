cc60b3e5c59fddc37a2e9d734aca92f2b263a4d0
MrFix1989 <madoc1989@gmail.com>
2019-12-20 21:38:58 +0200

    6.3  Домашка по строкам

--- app.js
+++ app.js
@@ -1,28 +1,5 @@
-const firstName = 'Avganistan';
-const lastName = 'Borisov';
-const age = 30;
+let string = 'some test string';
 
-let str;
+//1. Получить первую и последнюю буквы строки
 
-str = '<ul>' +
-        '<li>' + firstName + '</li>'+
-        '<li>' + lastName + '</li>'+
-        '<li>' + age + '</li>'+
-    '</ul>';
-
-
-//console.log(str);
-//document.writeln(str);
-
-str = `<ul>
-            <li>First Name: ${firstName}</li>
-            <li>Last name: ${lastName}</li>
-            <li>Age: ${age}</li>
-            <li>Math: ${Math.random()}</li>
-            <li>5 + 5: ${5 + 5}</li>
-        </ul>
-`;
-
-//console.log(str);
-
-document.body.innerHTML = str; // интеграция в тело HTML страници
+console.log();
