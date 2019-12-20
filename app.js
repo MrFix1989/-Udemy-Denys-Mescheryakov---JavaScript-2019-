b8e894d3d0235f1ed81f506ccf65dc60d9bf2992
MrFix1989 <madoc1989@gmail.com>
2019-12-20 21:31:52 +0200

    6.2 Новый вариант записи

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ str = '<ul>' +
     '</ul>';
 
 
-console.log(str);
+//console.log(str);
 //document.writeln(str);
 
+str = `<ul>
+            <li>First Name: ${firstName}</li>
+            <li>Last name: ${lastName}</li>
+            <li>Age: ${age}</li>
+            <li>Math: ${Math.random()}</li>
+            <li>5 + 5: ${5 + 5}</li>
+        </ul>
+`;
+
+//console.log(str);
+
+document.body.innerHTML = str; // интеграция в тело HTML страници
